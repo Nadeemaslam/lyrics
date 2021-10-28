91ca710858a7dfb0d4122bbcfef66709ceaec840
AhmadNomaan <43290231+AhmadNomaan@users.noreply.github.com>
2021-10-28 11:16:08 +0530

    changed url pattern for article
    changed from path to re_path

--- lyricapp/urls.py
+++ lyricapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path('explore/', views.explore, name='explore'),
     path('contact/', views.contact, name='contact'),
     path('results/', views.search_results, name='search'),
-    path('article/(?P<slug>[\w-]+)/$', views.article, name='article')
+    re_path(r'^article/(?P<slug>[\w-]+)/$', views.article, name='article')
 ]
